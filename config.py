44b2a631465af30ae021d7dddcd94dd4c69c84ee
Aridai Bordón <aridaybordon@gmail.com>
2022-07-11 13:48:54 +0100

    update: add more customization to backup

--- config.py
+++ config.py
@@ -8,5 +8,7 @@ dotenv.load_dotenv()
 config = {
     "PUBLIC_ADDRESS": os.environ["PUBLIC_ADDRESS"],
     "PARTIAL_PRIVATE_KEY": os.environ["PARTIAL_PRIVATE_KEY"],
-    "UNKNOWN_CHAR": "X"   
+    "UNKNOWN_CHAR": "X",
+    "CREATE_BACKUP": True,
+    "PERIOD_BACKUP": int(16**4)
 }
